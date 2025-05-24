6fb421eccda4c6e373a7ec3b481932b084c932e3
minesyw <81350664+shelter009@users.noreply.github.com>
2025-05-24 14:39:28 +0800

    完成了 lab4

---
+++ lab4/flik/FilkTest.java
@@ -0,0 +1,13 @@
+package flik;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FilkTest {
+    @Test
+    public void TestEqual(){
+        for (int i = 0; i < 400; i++){
+            System.out.println(i);
+            assertTrue(Flik.isSameNumber(i, i));
+        }
+    }
+}
