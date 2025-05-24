dd3c6277c88a2144b7c6ce535aa4947d3d15d265
minesyw <81350664+shelter009@users.noreply.github.com>
2025-05-24 14:44:50 +0800

    风格问题：lab4 在 { 前面加上空格

--- lab4/flik/FilkTest.java
+++ lab4/flik/FilkTest.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.*;
 
 public class FilkTest {
     @Test
-    public void TestEqual(){
-        for (int i = 0; i < 400; i++){
+    public void TestEqual() {
+        for (int i = 0; i < 400; i++) {
             System.out.println(i);
             assertTrue(Flik.isSameNumber(i, i));
         }
