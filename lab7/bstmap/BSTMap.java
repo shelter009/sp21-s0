718ff103b39facc9fc480afce62f5dc6278c39dd
minesyw <81350664+shelter009@users.noreply.github.com>
2025-06-01 22:07:40 +0800

    初步完成lab7, remove方法待完成

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,124 @@
+package bstmap;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
+    private Node root;
+    private int size;
+
+    @Override
+    public void clear() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return researchKey(root, key) != null;
+    }
+
+    public V researchKey(Node node, K k){
+        if (node == null){
+            return null;
+        }
+        if (node.key == k){
+            return node.val;
+        }
+        else if (node.key.compareTo(k) > 0){
+            return researchKey(node.left, k);
+        }
+        else if (node.key.compareTo(k) < 0)
+            return researchKey(node.right, k);
+        return null;
+    }
+
+
+    @Override
+    public V get(K key) {
+        if ( !containsKey(key) )
+            return null;
+        else{
+            return researchKey(root, key);
+        }
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        root = setItem(root, key, value);
+    }
+    public Node setItem(Node n, K key, V val){
+        if (n == null){
+            return new Node(key, val);
+        }
+        if (n.key.compareTo(key) == 0){
+            n.val = val;
+        }
+        else if (n.key.compareTo(key) > 0){
+            n.left = setItem(n.left, key, val);
+        }
+        else if (n.key.compareTo(key) < 0)
+            n.right = setItem(n.right, key, val);
+        return n;
+    }
+
+    public void midOrder(Node node, ArrayList<K> L){
+        if (node == null)
+            return;
+        midOrder(node.left, L);
+        L.add(node.key);
+        midOrder(node.right, L);
+    }
+
+    @Override
+    public Set<K> keySet() {
+        ArrayList<K> L = new ArrayList<>();
+        midOrder(root, L);
+        return Set.copyOf(L);
+    }
+
+    public void printOrder(){
+        for(K i : this){
+            System.out.println(i);
+        }
+    }
+
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        ArrayList<K> L = new ArrayList<>();
+        midOrder(root, L);
+        return L.iterator();
+    }
+
+    private class Node{
+        private K key;
+        private V val;
+        private Node left;
+        private Node right;
+        private Node(K key, V val){
+            this.key = key;
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+}
---
+++ lab7/bstmap/BSTTest.java
@@ -0,0 +1,16 @@
+package bstmap;
+
+import org.junit.Test;
+
+public class BSTTest {
+    @Test
+    public void TestPrint(){
+        BSTMap<String, Integer> Map = new BSTMap<>();
+        Map.put("dsfdf", 122);
+        Map.put("wd", 45);
+        Map.put("ad", 56);
+        Map.put("hgh", 83);
+        Map.put("ty", 34);
+        Map.printOrder();
+    }
+}
